6a2901868420d97fac5d4531e2914b6f6e0ad548
Milad Ariany <milad.ariany@sap.com>
2022-12-25 17:22:01 +0000

    Create a chatroom to receive and broadcast messages between its clients

---
+++ examples/chatroom/README.md
@@ -0,0 +1,4 @@
+## Chat room
+The chat room lets several clients broadcast textual messages to each other.
+
+o see the related tutorial in Farsi, click on this link: https://youtu.be/hZBmmqEDsSY
---
+++ examples/chatroom/room/room.go
@@ -0,0 +1,85 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Room is a server that lets clients chat with each other.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+type client chan<- string // an outgoing message channel
+
+// In below we define some channels of channels
+var (
+	entering = make(chan client) // a channel of channels
+	leaving  = make(chan client) // a channel of channels
+	messages = make(chan string) // all incoming client messages
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg // What if a reader fails or is slow?
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "Welcome to the room " + who // greetings to the client
+	messages <- who + " join the room"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- ch
+	messages <- who + " has left the room"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
