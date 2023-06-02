b3bd23c275fb519680e9a9f3376f240b7ab59ac2
Milad Ariany <milad.ariany@sap.com>
2023-06-02 17:20:09 +0000

    limit number of goroutines in a for-loop

---
+++ channels/buffered/limitgoroutins/channel.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sync"
+)
+
+func main() {
+
+	files := make(chan int)
+	dirs := make(chan string)
+	tokens := make(chan struct{}, 10)
+
+	wg := &sync.WaitGroup{}
+
+	countFiles := func(dirPath string) {
+
+		defer func() {
+			wg.Done()
+			<-tokens
+		}()
+
+		entries, err := ioutil.ReadDir(dirPath)
+		if err != nil {
+			return
+		}
+
+		for _, entry := range entries {
+			if entry.IsDir() {
+				dirs <- fmt.Sprintf("%s/%s", dirPath, entry.Name())
+			} else {
+				files <- 1
+			}
+		}
+	}
+
+	go func() {
+		for dir := range dirs {
+			wg.Add(1)
+			go func(dir string) {
+				tokens <- struct{}{}
+				countFiles(dir)
+			}(dir)
+		}
+	}()
+
+	root := "."
+	dirs <- root
+
+	go func() {
+		wg.Wait()
+		close(files)
+		close(dirs)
+	}()
+
+	cnt := 0
+	for i := range files {
+		cnt += i
+	}
+
+	fmt.Printf("Number of files in the %s directory and its subdirectories: %d \n", root, cnt)
+}
